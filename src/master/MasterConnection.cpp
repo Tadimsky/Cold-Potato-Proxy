6d1d882fca3ff12702e42b0978ab1b5c0b10b9a8
Elder Massahiro Yoshida <emy2@duke.edu>
2015-04-25 12:21:56 -0400

    Filling in Connection

---
+++ src/master/MasterConnection.cpp
@@ -0,0 +1,73 @@
+//
+//  MasterConnection.cpp
+//  Cold-Potato-Proxy
+//
+//  Created by Elder Yoshida on 4/25/15.
+//  Copyright (c) 2015 Elder Yoshida. All rights reserved.
+//
+
+#include "MasterConnection.h"
+#include "Constants.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+MasterConnection::MasterConnection(ConnectionData* connection) : Connection(connection) {
+}
+
+void MasterConnection::handleConnection() {
+//	int sock = mConnectionData->socket;
+//	
+//	mSock = std::make_unique<Socket>(sock);
+//	
+//	AddressDetails request;
+//	if (!this->handleRequest(request)) {
+//		return;
+//	}
+}
+
+bool MasterConnection::verifyVersion(char version) {
+//	if (version != Constants::Relay::Version::V1) {
+//		cerr << "Invalid relay version." << endl;
+//		return false;
+//	}
+	return true;
+}
+
+
+bool MasterConnection::handleRequest(AddressDetails & request) {
+	bytes header;
+	
+	if (!mSock->receive(header, 3))
+	{
+		return false;
+	}
+	
+	if (header.size() != 3)
+	{
+		return false;
+	}
+	
+	if (!this->verifyVersion(header[0])) {
+		return false;
+	}
+	
+	// We only support TCP CONNECT, so fail other commands.
+	if (header[1] != Constants::Relay::Command::TCPConnection)
+	{
+		cerr << "Unsupported command: " << hex << header[1] << endl;
+		// use this namespace for easier to read messages.
+		using namespace Constants::Messages::Relay::Request::Response;
+		// TODO: Process error messages better. + anything else??
+		mSock->send(InvalidConnection +  Blank);
+		
+		return false;
+	}
+	
+	return this->readAddressInformation(request);
+}
+
+MasterConnection::~MasterConnection() {
+	//delete mConnectionData;
+}
