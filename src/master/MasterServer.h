4e52e85eb407c05d8f1d319c08a3fb1d81b86e04
Elder Massahiro Yoshida <emy2@duke.edu>
2015-04-25 12:21:08 -0400

    implementing masterserver.h

--- src/master/MasterServer.h
+++ src/master/MasterServer.h
@@ -9,6 +9,13 @@
 #ifndef __Cold_Potato_Proxy__MasterServer__
 #define __Cold_Potato_Proxy__MasterServer__
 
-#include <stdio.h>
+#include "ListenServer.h"
+
+class MasterServer : public ListenServer{
+public:
+	MasterServer(int port) : ListenServer(port) {};
+	
+	virtual void processConnection(ConnectionData *data) override;
+};
 
 #endif /* defined(__Cold_Potato_Proxy__MasterServer__) */
